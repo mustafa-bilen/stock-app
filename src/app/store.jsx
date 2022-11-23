6350db77772691ad72dbd1e753d4bb98df6d5bc7
mustafa-bilen <mustafabilen6669@gmail.com>
2022-11-24 00:35:29 +0300

    .

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/StockSlice";
 import storage from "redux-persist/lib/storage/"; // defaults to localStorage for web
 
 import {
@@ -22,6 +23,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock: stockReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
