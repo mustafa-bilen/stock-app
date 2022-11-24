ac45c5d7407093074e3c254ee5e8f84b8771a35a
mustafa-bilen <mustafabilen6669@gmail.com>
2022-11-24 16:31:07 +0300

    useStock

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/StockSlice";
 
 const Firms = () => {
+  const dispatch = useDispatch();
+
   const { token } = useSelector((state) => state.auth);
   const BASE_URL = "https://13602.fullstack.clarusway.com/";
   const getFirms = async () => {
+    const url = "firms";
+    dispatch(fetchStart());
     try {
       const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
         headers: { Authorization: `Token ${token}` },
       });
 
       console.log(data);
+      dispatch(getSuccess({ data, url }));
     } catch (error) {
+      dispatch(fetchFail);
       console.log(error);
     }
   };
